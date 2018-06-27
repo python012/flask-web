fbbaad41a40b6a6cbe04409783b290c283e3b3d7
Reed Xia <huaqin2005@gmail.com>
2018-06-27 22:26:36 +0800

    update app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def hello():
+    return 'Hello Flask!'
+
+
+@app.route('/num/<num>/')
+def show_num(num):
+    return 'The number is {}'.format(num)
+
+
+if __name__ == '__main__':
+    app.run()
