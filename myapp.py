165636215fb07cb2155a5162a3825b3994eb3114
Reed Xia <huaqin2005@gmail.com>
2018-08-29 22:24:59 +0800

    add dependency python-dotenv, make some changes

--- myapp.py
+++ myapp.py
@@ -1,19 +1,16 @@
 # coding=utf-8
 from flask import Flask
 
+
 app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route('/')
-def hello():
-    return 'Hello Flask!'
+@app.route('/geet/<name>')
+def hello(name):
+    return '<h1>Hello %s!</h1>' % name
 
 
 @app.route('/num/<num>/')
 def show_num(num):
     return 'The number is {}'.format(num)
-
-
-if __name__ == '__main__':
-    app.run()
